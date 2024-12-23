14e113a1f82772e4f20657c561a0b23082aaffb3
HemantJatal <hemantjatal59@gmail.com>
2024-12-23 16:31:51 +0530

    init:remix-jira-clone

--- Hemant-Jatal/remix-jira-clone/app/lib/getUser.ts
+++
@@ -1,6 +0,0 @@
-import { getSession } from "~/lib/session.server";
-
-export const getUser = async (request: Request) => {
-  const session = await getSession(request.headers.get("Cookie"));
-  return session.data;
-};
--- Hemant-Jatal/remix-jira-clone/prisma/migrations/20241031193125_new/migration.sql
+++
@@ -1,5 +0,0 @@
--- CreateEnum
-CREATE TYPE "Provider" AS ENUM ('google', 'github', 'form');
-
--- AlterTable
-ALTER TABLE "User" ADD COLUMN     "provider" "Provider" NOT NULL DEFAULT 'form';
